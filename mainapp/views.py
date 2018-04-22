44c979675b752640ab366ff014e4216bb7b0ce3a
Pavel Vishnyakov <djhumster@gmail.com>
2018-04-22 21:18:24 +0300

    меню

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+links_menu = [
+    {'href': 'index', 'name': 'главная'},
+    {'href': 'category', 'name': 'телефоны'},
+    {'href': 'category', 'name': 'ноутбуки'},
+    {'href': 'category', 'name': 'телевизоры'},
+    {'href': 'category', 'name': 'игровые приставки'},
+    {'href': 'category', 'name': 'игры'},
+    {'href': 'contacts', 'name': 'контакты'},
+]
+
 def index_view(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', {'links_menu': links_menu})
     
 def category_view(request):
-    return render(request, 'mainapp/category.html')
+    return render(request, 'mainapp/category.html', {'links_menu': links_menu})
 
 def contacts_view(request):
-    return render(request, 'mainapp/contacts.html')
+    return render(request, 'mainapp/contacts.html', {'links_menu': links_menu})
