cd5a81dc9419640a34cc9a20cb8f2b109aeeabed
Pavel Vishnyakov <djhumster@gmail.com>
2018-05-30 13:35:33 +0300

    упростил форматирование цен

--- mainapp/templatetags/extra_tags.py
+++ mainapp/templatetags/extra_tags.py
@@ -1,14 +1,8 @@
-from textwrap import wrap
-
 from django import template
 
 register = template.Library()
 
 @register.filter(name='pprice')
 def pretty_price(value):
-    """ Разбивка отображения цен по 3 символа: 12 345,00"""
-    s = str(value)
-    tmp = s[:-3]
-    tmp = ' '.join(wrap(tmp[::-1], 3))[::-1]
-
-    return tmp + ',' + s[-2:]
+    """Разбивка отображения цен по 3 символа: 12 345,00"""
+    return f'{value:,}'.replace(',', ' ').replace('.', ',')
